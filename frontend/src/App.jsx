93cc0772cf44df3929862b862a0507f2ced8c75c
salemHb <husseinsalim419@gmail.com>
2025-06-02 16:35:58 +0300

    final touches

---
+++ frontend/src/App.jsx
@@ -0,0 +1,23 @@
+import { Route, Routes } from "react-router-dom";
+import "./css/App.css";
+import MovieCard from "./components/MovieCard";
+import Home from "./pages/Home";
+import Favorites from "./pages/Favorites";
+import NavBar from "./components/NavBar";
+import { MovieProvider } from "./context/MovieContext";
+
+function App() {
+	return (
+		<MovieProvider>
+			<NavBar />
+			<main className="main-content">
+				<Routes>
+					<Route path="/" element={<Home />} />
+					<Route path="/favorites" element={<Favorites />} />
+				</Routes>
+			</main>
+		</MovieProvider>
+	);
+}
+
+export default App;
